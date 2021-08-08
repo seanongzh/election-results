649fecf6d01376372c862f476eb97dd20c19d604
seanongzh <26339200+seanongzh@users.noreply.github.com>
2021-08-08 14:19:07 +0800

    Update to match new col names in db

--- scripts/add_candidate.py
+++ scripts/add_candidate.py
@@ -1,5 +1,5 @@
 # Script to add a single candidate into the candidates table of the database
-# 1. Table needs "first_name" and "last_name" columns
+# 1. Table needs "first" and "last" columns for first and last names
 # 2. Script will check for duplicates and not insert duplicates
 # 
 # USAGE: add_candidate.py [First Name] [Last Name]
@@ -10,12 +10,12 @@ import sys
 conn = psycopg2.connect("dbname=results user=sean password=sean")
 cur = conn.cursor()
 
-cur.execute("select * from candidates where first_name=%s and last_name =%s",
+cur.execute("select * from candidates where first=%s and last =%s",
             (sys.argv[1], sys.argv[2]))
 
 if (cur.fetchone() == None):
-  cur.execute("insert into candidates(first_name, last_name) values(%s, %s)",
-              (sys.argv[1], sys.argv[2]))
+  cur.execute("insert into candidates(first, last, full_name) values(%s, %s, %s)",
+              (sys.argv[1], sys.argv[2], sys.argv[1]+" "+sys.argv[2]))
   print("Inserted")
   conn.commit()
 else:
