1de5af2ec309c4f553f35e22ac7fb1375c174438
seanongzh <26339200+seanongzh@users.noreply.github.com>
2022-03-08 21:19:11 +0800

    Remove development secret key

--- web/settings.py
+++ web/settings.py
@@ -19,13 +19,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=-(#&fyz7mlmwl6gaxuemgh43$2r%@cwtw^wi)2)o$tl&j-8(a'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0']
 
 
 # Application definition
