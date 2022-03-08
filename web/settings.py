ec3b0ba3f6bf8aa2c068a8b6eb07de13f2768b62
seanongzh <26339200+seanongzh@users.noreply.github.com>
2022-03-08 21:04:49 +0800

    Automatic configuration with django-on-heroku

--- web/settings.py
+++ web/settings.py
@@ -129,3 +129,7 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# DJANGO SETTINGS
+import django_on_heroku
+django_on_heroku.settings(locals())
