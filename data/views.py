69a459d5c485041c844964074c495c2b1bb035c6
seanongzh <26339200+seanongzh@users.noreply.github.com>
2022-03-18 19:48:55 +0800

    Updated view to use base template

--- data/views.py
+++ data/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def home(request):
-    return HttpResponse("This is the main page for the Kenilworth Election Results database.")
+    return render(request, 'data/base.html')
