9b5833d9374aa79221044c4c02c690d295947707
sean <26339200+seanongzh@users.noreply.github.com>
2023-08-07 18:12:34 +0800

    Managing CSRF support

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = False
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['https://election-results.up.railway.app/']
+CSRF_TRUSTED_ORIGINS = ['https://election-results.up.railway.app/']
 CSRF_COOKIE_SECURE = True
 
 # Application definition
