96a6c99dd61b0f159289b93bdee273619d413179
sean <26339200+seanongzh@users.noreply.github.com>
2023-08-07 18:51:35 +0800

    Modify static files setting

--- config/settings.py
+++ config/settings.py
@@ -129,7 +129,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = 'home/sean/election-results/static'
+STATIC_ROOT = str(BASE_DIR / 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
